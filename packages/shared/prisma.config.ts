10c243a61bf0adf671a183c3c1fd8a84634828af
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2025-12-14 18:42:26 +0200

    use example env

--- packages/shared/prisma.config.ts
+++ packages/shared/prisma.config.ts
@@ -1,6 +1,14 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import fs from 'fs';
 import type { PrismaConfig } from 'prisma';
 import { env } from 'prisma/config';
+
+if (fs.existsSync('.env')) {
+  dotenv.config({ path: '.env' });
+} else {
+  // Prisma 7+ requires a DATABASE_URL to be set, so we load the example env file if no .env file is found
+  dotenv.config({ path: '.env.example' });
+}
 
 export default {
   schema: 'prisma/schema.prisma',
